28cb7a0ae04a0f4ba3eb6073352cbca5a2d82c80
Gabriel <leuping@gmail.com>
2019-05-26 19:34:16 -0700

    slicing indexer

---
+++ deprecated_slicing_indexer.py
@@ -0,0 +1,3 @@
+def plot_selected(df, columns, start_index, end_index):
+    """Plot the desired columns over index values in the given range."""
+    plot_data(df.ix[start_index:end_index, columns], "Selected")
